e737f79e9c52a34046781193903bfac53f0e469b
Daniel Brownlow <me@danbrownlow.co.uk>
2014-08-02 20:38:50 +0100

    Fixed responsive gallery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 $(document).foundation();
+
+// work hover
+$('.project').mouseover(function(){
+    //show the box
+    $(this).children('.project-hover-text').stop().animate({opacity:1},300);
+});
+
+$('.project').mouseleave(function(){
+    //hide the box
+    $(this).children('.project-hover-text').stop().animate({opacity:0},300);
+});
